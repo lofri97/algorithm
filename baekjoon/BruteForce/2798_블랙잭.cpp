464319f1774efb95d23395d099174bf9e63c13e6
lofri97 <lofri97@gmail.com>
2022-01-01 19:48:59 +0900

    boj

---
+++ baekjoon/BruteForce/2798_블랙잭.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int n, m;
+	int arr[100];
+
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int result = 0;
+	for (int i = 0; i < n -2 ; i++) {
+		for (int j = i + 1; j < n - 1; j++) {
+			for (int k = j + 1; k < n; k++) {
+				if ((arr[i] + arr[j] + arr[k]) > result &&
+					arr[i] + arr[j] + arr[k] <= m) {
+					result = arr[i] + arr[j] + arr[k];
+					if (result == m) {
+						printf("%d\n", result);
+						return 0;
+					}
+				}
+			}
+		}
+	}
+	printf("%d\n", result);
+	return 0;
+}
