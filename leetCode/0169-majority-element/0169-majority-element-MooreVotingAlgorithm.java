3ee56cf353fc889f5aa87e1e79a116b5f771e275
Lofri <73948418+lofri97@users.noreply.github.com>
2024-09-13 22:23:28 +0900

    Time: 2 ms (76.34%), Space: 53 MB (41.04%)

---
+++ leetCode/0169-majority-element/0169-majority-element-MooreVotingAlgorithm.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int result = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                result = nums[i];
+            }
+
+            if (nums[i] != result) {
+                count--;   
+            } else {
+                count++;
+            }
+        }
+        return result;
+    }
+}
