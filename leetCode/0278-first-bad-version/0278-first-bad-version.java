da7b1aef63c12ca0123f0d20d3228bd09892615f
Lofri <73948418+lofri97@users.noreply.github.com>
2024-09-21 23:12:35 +0900

    Time: 50 ms (6.2%), Space: 40.7 MB (11.26%)

---
+++ leetCode/0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 0, r = n;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (isBadVersion(m) == true && isBadVersion(m - 1) == false) {
+                return m;
+            }
+            else if (isBadVersion(m) == false) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return -1;
+    }
+}
