263a6c9d9ae6c4cfb3eb92848a4150d00db0ac3a
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-16 20:26:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 51.11 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,46 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
-        Map<Character, Character> map = new HashMap<>();
-        map.put('}', '{');
-        map.put(')', '(');
-        map.put(']', '[');
-        
-        for (int i = 0; i < s.length(); i++) {
-            Stack<Character> st = new Stack<>();
-            boolean flag = false;
-            
-            for (int j = i; j < s.length() + i; j++) {
-                int idx = j % s.length();
-                char target = s.charAt(idx);
-                if (st.isEmpty() && map.containsKey(target)) {
-                    flag = true;
-                    break;
-                }
-                if (st.isEmpty()) {
-                    st.add(target);
-                } else if (map.containsKey(target)) {
-                    if (st.peek() != map.get(target)) {
-                        flag = true;
-                        break;
-                    }
-                    st.pop();
-                } else {
-                    st.add(target);
-                }
-            }
-            
-            if (!flag && st.isEmpty()) {
-                answer++;
-            }
-        }
+        int answer = -1;
         return answer;
     }
 }
-
-/**
-s1945
-**/
