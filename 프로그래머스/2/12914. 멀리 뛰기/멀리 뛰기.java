98d980a7d191b04df1d1f07656c75b617e541cd1
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-16 16:08:31 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return n;
+        if (n == 2) return n;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        return dp[n];
+    }
+}
