4cb04eb1b58e22fa3e050dbb36ae3fdab0dd42b2
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-16 21:47:32 +0900

    [level 2] Title: H-Index, Time: 0.93 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        int answer = citations.length;
+        
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= answer)
+                break;
+            answer--;
+        }
+        return answer;
+    }
+}
