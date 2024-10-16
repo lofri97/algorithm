d67408e4e163526aa51cd1666ac4472b1cf850a9
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-17 00:18:13 +0900

    [level 2] Title: 튜플, Time: 82.90 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,33 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        List<int[]> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
         
-        StringBuilder sb = new StringBuilder();
-        List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char target = s.charAt(i);
-            if (Character.isDigit(target)) {
-                sb.append(target);
-            } 
-            else if (target == ',' && Character.isDigit(s.charAt(i-1))) {
-                temp.add(Integer.valueOf(sb.toString()));
-                sb.delete(0, sb.length());
-            } 
-            else if (target == '}' && Character.isDigit(s.charAt(i-1))) {
-                temp.add(Integer.valueOf(sb.toString()));
-                list.add(temp.stream().mapToInt(v -> v).toArray());
-                sb.delete(0, sb.length());
-                temp.clear();
-            }
-        }
-        Collections.sort(list, (v1, v2) -> v1.length - v2.length);
-        int[] answer = new int[list.size()];
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < list.size(); i++) {
-            for (int val : list.get(i)) {
-                if (set.add(val)) {
-                    answer[i] = val;
+        String[] arr = s.replaceAll("\\{", " ").replaceAll("\\}", " ").trim().split(" , ");
+        Arrays.sort(arr, (v1, v2) -> v1.length() - v2.length());
+        
+        int[] answer = new int[arr.length];
+        int i = 0;
+        for (String v1 : arr) {
+            for (String v2 : v1.split(",")) {
+                if (set.add(v2)) {
+                    answer[i++] = Integer.parseInt(v2);
                 }
             }
         }
