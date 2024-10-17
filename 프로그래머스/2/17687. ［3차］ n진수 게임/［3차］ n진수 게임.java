a746fdda587def1db24526694a047162ccc164cc
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-17 13:39:23 +0900

    [level 2] Title: [3차] n진수 게임, Time: 29.35 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,41 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        StringBuilder targetStr = new StringBuilder();
         
-        int curVal = 0;
-        int curIdx = 0;
-        while (sb.length() < t) {
-            char[] chars = getChars(curVal, n);
-            
-            for (int i = 0; i < chars.length && sb.length() < t; i++) {
-                if (curIdx % m == p - 1) {
-                    sb.append(chars[i]);
-                }
-                curIdx++;
-            }
-            curVal++;
-        }
-        return sb.toString();
-    }
-    
-    char[] getChars(int val, int n) {
-        StringBuilder sb = new StringBuilder();
-        
-        if (val == 0) {
-            return new char[] {'0'};
+        int start = 0;
+        while (targetStr.length() < m * t) {
+            targetStr.append(Integer.toString(start++, n));
         }
         
-        while (val > 0) {
-            int mod = val % n;
-            if (mod > 9) {
-                sb.append((char) ('A' + mod - 10));
-            } else {
-                sb.append(mod);
-            }
-            val /= n;
+        for (int i = p - 1; answer.length() < t; i += m) {
+            answer.append(targetStr.charAt(i));
         }
-        return sb.reverse().toString().toCharArray();
+        return answer.toString().toUpperCase();
     }
 }
