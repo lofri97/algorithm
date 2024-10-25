649f2a784cca5084d9e0e566a5617ec4d44e6cd5
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-25 22:21:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.12 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,21 +5,15 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         
         for (String name : completion) {
-            Integer count = map.getOrDefault(name, 0);
-            map.put(name, ++count);
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        for (String name : participant) {
-            if (!map.containsKey(name)) {
-                return name;
-            }
-            if (map.get(name) == 1) {
-                map.remove(name);
-                continue;
-            }
-            map.put(name, map.get(name) - 1);
-            
+        for (String name: participant) {
+            if (!map.containsKey(name)) return name;
+            int count = map.get(name);
+            if (count == 0) return name;
+            map.put(name, count - 1);
         }
-        return "error";
+        return "";
     }
 }
