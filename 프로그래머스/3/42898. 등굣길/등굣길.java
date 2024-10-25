a8c7357d03cd42042a18cbbe784e6948b5360346
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-26 02:09:38 +0900

    [level 3] Title: 등굣길, Time: 0.93 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,21 +1,18 @@
 class Solution {
-    
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n][m];
         
-        dp[0][0] = 1;
-        
         for (int[] puddle : puddles) {
-            dp[puddle[1] - 1][puddle[0]- 1] = -1;
+            dp[puddle[1] - 1][puddle[0] - 1] = -1;
         }
-               
+        
+        dp[0][0] = 1;
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < m; x++) {
                 if (dp[y][x] == -1) {
                     dp[y][x] = 0;
                     continue;
                 }
-                
                 if (y > 0) {
                     dp[y][x] += dp[y-1][x];
                 }
