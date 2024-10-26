9e1cbc844d08dcca84790d79dbcfeea0df375d8a
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-26 20:26:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 50.66 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+        
+        
+        char[] arr = s.toCharArray();
+        for (int j = 0; j < s.length(); j++) {
+            Stack<Character> st = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                
+                char val = arr[(i + j) % s.length()];
+                if (val == '[' || val == '{' || val == '(') {
+                    st.add(val);
+                } else if (val == ']') {
+                    if (st.isEmpty() || st.peek() != '[') break;
+                    st.pop();
+                } else if (val == '}') {
+                    if (st.isEmpty() || st.peek() != '{') break;
+                    st.pop();
+                } else if (val == ')') {
+                    if (st.isEmpty() || st.peek() != '(') break;
+                    st.pop();
+                }
+                if (st.isEmpty() && i == s.length() - 1) answer++;
+            }
+        }
+        
         return answer;
     }
 }
