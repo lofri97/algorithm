2326d85cbbd94071cdbce77ea958a5ed1678e99d
Lofri <73948418+lofri97@users.noreply.github.com>
2024-10-26 20:16:17 +0900

    [level 2] Title: 올바른 괄호, Time: 6.48 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,16 @@
 class Solution {
     boolean solution(String s) {
-        int flag = 0;
+        int count = 0;
         
         for (char c : s.toCharArray()) {
-            if (c == '(') {
-                flag++;
-            } else {
-                flag--;
-            }
-            if (flag < 0) {
+            if (c == '(')
+                count++;
+            else if (c == ')')
+                count--;
+            
+            if (count < 0)
                 return false;
-            }
         }
-        return flag == 0;
+        return count == 0;
     }
 }
