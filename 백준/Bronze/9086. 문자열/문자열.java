1f812de13a873a95cbde228a52cd0292a795ceda
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-01 21:17:00 +0900

    [Bronze V] Title: 문자열, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            System.out.println(String.valueOf(str.charAt(0)) + String.valueOf(str.charAt(str.length()-1)));
+        }
+    }
+}
