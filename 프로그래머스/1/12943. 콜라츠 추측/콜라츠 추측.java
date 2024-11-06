0b4904b583eb67b7fbb86755fe2f93bb733db513
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-07 01:11:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        long n = num;
+        
+        while (n != 1 && answer < 500) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            answer++;
+        }
+        return n == 1 ? answer : -1;
+    }
+}
