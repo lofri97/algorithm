f6094d8308495e5fba1017cc74c5d88e85895de8
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-10 21:42:36 +0900

    [level 1] Title: 추억 점수, Time: 2.28 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        int[] answer = new int[photo.length];
+        
+        for (int i = 0; i < photo.length; i++) {
+            for (String p : photo[i]) {
+                answer[i] += map.getOrDefault(p, 0);
+            }
+        }
+        return answer;
+    }
+}
