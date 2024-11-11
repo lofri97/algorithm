7e6f3437f7e89c0ea0e3994c5a8b3fc77aae8b2a
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-11 20:05:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 120.06 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,21 +1,6 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        
-        for (int i = 1; i <= number; i++) {
-            answer += calc(i, limit, power);
-        }
         return answer;
-    }
-    
-    int calc(int number, int limit, int power) {
-        int count = 0;
-        
-        for (int i = 1; i * i<= number; i++) {
-            if (i * i == number) count++;
-            else if (number % i == 0) count += 2;
-            if (count > limit) return power;
-        }
-        return count;
     }
 }
