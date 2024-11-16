7cb6672a203c635af0572d8e5887ff466d39966b
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-17 00:28:21 +0900

    [D3] Title: 준환이의 운동관리, Time: 314 ms, Memory: 32,332 KB -BaekjoonHub

---
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int tt = Integer.parseInt(br.readLine());
+        
+        for (int t = 0; t < tt; t++) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            
+            if (arr[0] > arr[2]) {
+                System.out.printf("#%d %d\n", t + 1, arr[0] - arr[2]);
+            } else if (arr[1] < arr[2]) {
+                System.out.printf("#%d %d\n", t + 1, -1);
+            } else {
+                System.out.printf("#%d %d\n", t + 1, 0);
+            }
+        }
+    }
+}
