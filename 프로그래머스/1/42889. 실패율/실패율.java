5c35c2a240cd464b0450530eae9ad3ce51d33491
Lofri <73948418+lofri97@users.noreply.github.com>
2024-11-21 01:37:13 +0900

    [level 1] Title: 실패율, Time: 8.84 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] counts = new int[N+2];
+        int[] sums = new int[N+2];
+        
+        for (int stage : stages) {
+            counts[stage]++;
+        }
+        
+        sums[N+1] = counts[N+1];
+        for (int i = N; i >= 1; i--) {
+            sums[i] = counts[i] + sums[i+1];
+        }
+        
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            answer.add(i);
+        }
+        
+        Collections.sort(answer, (v1, v2) -> {
+            double f1 = counts[v1] / (double) sums[v1];
+            double f2 = counts[v2] / (double) sums[v2];
+                         
+            if (f1 == f2) {
+                return (int) (v1 - v2);
+            } else {
+                return f2 - f1 < 0 ? -1 : f2 - f1 > 0 ? 1 : 0;
+            }
+        });
+        
+        return answer.stream().mapToInt(v->v).toArray();
+    }
+}
