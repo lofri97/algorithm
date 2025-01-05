c76888542d359750beb63605d6a2e6e7e9b298d8
Lofri <73948418+lofri97@users.noreply.github.com>
2025-01-05 21:21:30 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,17 +1,11 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        if (date1[0] < date2[0]) {
-            return 1;
-        } else if (date1[0] > date2[0]) {
-            return 0;
-        } else if (date1[1] < date2[1]) {
-            return 1;
-        } else if (date1[1] > date2[1]) {
-            return 0;
-        } else if (date1[2] < date2[2]) {
-            return 1;
-        } else if (date1[1] > date2[2]) {
-            return 0;
+        for (int i = 0; i < 3; i++) {
+            if (date1[i] < date2[i]) {
+                return 1;
+            } else if (date1[i] > date2[i]) {
+                return 0;
+            }
         }
         return 0;
     }
