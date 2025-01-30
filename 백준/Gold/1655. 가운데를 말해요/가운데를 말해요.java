d9db1a5b16e445583856eb794478dc7b59bcbb31
Lofri <73948418+lofri97@users.noreply.github.com>
2025-01-31 00:27:49 +0900

    [Gold II] Title: 가운데를 말해요, Time: 436 ms, Memory: 33972 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -4,41 +4,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue1 = new PriorityQueue<>((v1, v2) -> v2 - v1);
+        Queue<Integer> queue2 = new PriorityQueue<>();
 
-        for (int t = 0; t < n; t++) {
-            int val = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-            int l = 0;
-            int r = list.size() - 1;
+        for (int i = 0; i<n; i++) {
+            int tmp = Integer.parseInt(br.readLine());
 
-            int result = -1;
-            while (l <= r) {
-                int m = (l + r) / 2;
-
-                if (list.get(m) >= val) {
-                    result = m;
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }
-            }
-
-            if (result == -1) {
-                list.add(val);
+            if (queue1.size() == queue2.size()) {
+                queue1.add(tmp);
             } else {
-                list.add(result, val);
+                queue2.add(tmp);
             }
 
-            if (list.size() == 1) {
-                sb.append(list.get(0));
-            } else {
-                sb.append(list.get((list.size() - 1) / 2));
+            if (queue1.size() > 0 && queue2.size() > 0 && queue1.peek() > queue2.peek()) {
+                int tmp1 = queue1.poll();
+                int tmp2 = queue2.poll();
+                queue1.add(tmp2);
+                queue2.add(tmp1);
             }
-            sb.append("\n");
+
+            sb.append(queue1.peek()).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
