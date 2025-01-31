75770218c3e921d43bff9e3e275f6e6386058968
Lofri <73948418+lofri97@users.noreply.github.com>
2025-02-01 00:32:28 +0900

    [Silver II] Title: 나무 자르기, Time: 964 ms, Memory: 119448 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = arr[n-1];
+        int result = - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long sum = 0;
+            for (int a : arr) {
+                sum += Math.max(a - mid, 0);
+            }
+            if (sum >= m) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
