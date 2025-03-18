ef4d8651f42aab4b296325b48586a9c7c9807672
Lofri <73948418+lofri97@users.noreply.github.com>
2025-03-18 19:37:42 +0900

    [Gold IV] Title: 최단경로, Time: 796 ms, Memory: 133432 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,44 +4,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(br.readLine());
 
-        int s = Integer.parseInt(br.readLine());
         Map<Integer, List<int[]>> graph = new HashMap<>();
+        int[] dist = new int[v + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
 
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-            List<int[]> edges = graph.getOrDefault(node, new ArrayList<>());
-            edges.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-            graph.put(node, edges);
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            List<int[]> edges = graph.getOrDefault(from, new ArrayList<>());
+            edges.add(new int[] {to, weight});
+            graph.put(from, edges);
         }
 
-        int[] dist = new int[v + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[s] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((v1, v2) -> v1[1] - v2[1]);
-
-        pq.offer(new int[] {s, 0});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((v1,v2)->v1[1]-v2[1]);
+        pq.add(new int[] {k, 0});
 
         while (!pq.isEmpty()) {
             int[] cur = pq.remove();
 
-            int curDist = cur[1];
-            int curNode = cur[0];
-
-            for (int[] edge : graph.getOrDefault(curNode, new ArrayList<>())) {
-                int nextNode = edge[0];
-                int nextDist = curDist + edge[1];
-
-                if (dist[nextNode] > nextDist) {
-                    dist[nextNode] = nextDist;
-                    pq.offer(new int[] {nextNode, nextDist});
+            for (int[] edge : graph.getOrDefault(cur[0], new ArrayList<>())) {
+                if (dist[edge[0]] > cur[1] + edge[1]) {
+                    dist[edge[0]] = cur[1] + edge[1];
+                    pq.offer(new int[] {edge[0], cur[1] + edge[1]});
                 }
             }
         }
